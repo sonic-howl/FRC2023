20e18f5be45f7276e12b6f64c9af95ad9fcc85ef
Neil <neil3524@gmail.com>
2023-03-22 15:30:01 -0400

    Make StowCommand into an InstantCommand (WIP, might revert)

--- commands/Claw/StowCommand.py
+++ commands/Claw/StowCommand.py
@@ -1,27 +1,46 @@
 import typing
-from commands2 import Command, Subsystem
+from commands2 import Command, Subsystem, InstantCommand
+from constants import ArmConstants
 from subsystems.Arm.ArmAssemblySubsystem import ArmAssemblySubsystem
 
 
-class StowCommand(Command):
-    def __init__(self, arm: ArmAssemblySubsystem) -> None:
+class StowCommand(InstantCommand):
+    def __init__(
+        self, arm: ArmAssemblySubsystem, gamePieceType=ArmConstants.GamePieceType.kEmpty
+    ) -> None:
         super().__init__()
         self.arm = arm
-        self.finished = False
-
-    def getRequirements(self) -> typing.Set[Subsystem]:
-        return {self.arm}
-
-    def initialize(self) -> None:
-        pass
+        self.gamePieceType = gamePieceType
 
     def execute(self) -> None:
-        # this only needs to execute once since it sets a reference angle to the spark max.
-        self.arm.stow()
-        self.finished = True
+        self.arm.stow(self.gamePieceType)
+        return super().execute()
 
-    def end(self, interrupted: bool) -> None:
-        pass
 
-    def isFinished(self) -> bool:
-        return self.finished
+# class StowCommand(Command):
+#     def __init__(
+#         self, arm: ArmAssemblySubsystem, gamePieceType=ArmConstants.GamePieceType.kEmpty
+#     ) -> None:
+#         super().__init__()
+
+#         self.arm = arm
+#         self.gamePieceType = gamePieceType
+
+#         self.finished = False
+
+#     def getRequirements(self) -> typing.Set[Subsystem]:
+#         return {self.arm}
+
+#     def initialize(self) -> None:
+#         pass
+
+#     def execute(self) -> None:
+#         # this only needs to execute once since it sets a reference angle to the spark max.
+#         self.arm.stow(self.gamePieceType)
+#         self.finished = True
+
+#     def end(self, interrupted: bool) -> None:
+#         pass
+
+#     def isFinished(self) -> bool:
+#         return self.finished
