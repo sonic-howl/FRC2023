eeb97c16b6331bdf5dc097aa0ceac4667757a8e3
Neil <neil3524@gmail.com>
2023-03-27 16:13:05 -0400

    Add ArmCommand abstract class to set arm angles

--- commands/Claw/FloorPickupCommand.py
+++ commands/Claw/FloorPickupCommand.py
@@ -2,15 +2,14 @@ from .ArmCommand import ArmCommand
 from constants import ArmConstants
 
 
-class StowCommand(ArmCommand):
+class FloorPickupCommand(ArmCommand):
     def initialize(self) -> None:
         armAngle = ArmConstants.angles[self.gamePieceType][
-            ArmConstants.AngleType.kStow
+            ArmConstants.AngleType.kFloor
         ][ArmConstants.SubsystemType.kArm]
 
         clawAngle = ArmConstants.angles[self.gamePieceType][
-            ArmConstants.AngleType.kStow
+            ArmConstants.AngleType.kFloor
         ][ArmConstants.SubsystemType.kClaw]
 
-        # TODO might have to add logic to avoid claw collisions using the arm's current angle.
         self.arm.setArmAndClawAngle(armAngle, clawAngle)
